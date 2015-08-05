8394b1ab6c16f2e498d79d38bf3072c4687056e2
diagonalwalnet <thadhoskins@live.com>
2015-08-05 11:23:28 +0200

    initial commit

---
+++ mljPodcast/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mljPodcast.MvcApplication" Language="C#" %>
---
+++ mljPodcast/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mljPodcast.Startup))]
+namespace mljPodcast
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

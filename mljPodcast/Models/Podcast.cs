88ee2c96be595568493e42c47f4495ad858f3df0
Thad Hoskins <thadhoskins@live.com>
2015-08-14 15:59:24 +0200

    A Little bit of everything.

--- mljPodcast/Models/Podcast.cs
+++ mljPodcast/Models/Podcast.cs
@@ -5,6 +5,7 @@ namespace mljPodcast
 {
     public class Podcast
     {
+        public int PodcastId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string BibleReference { get; set; }
@@ -12,28 +13,23 @@ namespace mljPodcast
         public DateTime? PublicationDate { get; set; }
         public double? Length { get; set; }
         public string EpisodeUri { get; set; }
-        public int PodcastCollectionid { get; set; }
-        public PodcastCollection Collection { get; set; }
+        public int PodcastCollectionId { get; set; }
+        public int Order { get; set; }
+
+        public virtual PodcastCollection PodcastCollection { get; set; }
     }
 
     public class PodcastCollection
     {
-        public int Id { get; set; }
+        public int PodcastCollectionId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Uri { get; set; }
-        public string ParentCollectionId { get; set; }
+        public int ParentCollectionId { get; set; }
+        public int Order { get; set; }
 
         public virtual PodcastCollection ParentCollection { get; set; }
-        public virtual ICollection<PodcastCollection> ChildCollections { get; set; }
-        public virtual ICollection<Podcast> Podcasts { get; set; }
+        public virtual List<PodcastCollection> ChildCollections { get; set; }
+        public virtual List<Podcast> Podcasts { get; set; }
     }
-
-    // <item>
-    //  <title>411 Item 138 ZedCast with Bruce Murray - Voicemail line 206-666-4357 </title>
-    //  <link>http://podcast411.com/forums/viewtopic.php?t=451</link>
-    //  <guid>http://media.libsyn.com/media/podcast411/411_060325.mp3</guid>
-    //  <description></description>
-    //  <enclosure url = "http://media.libsyn.com/media/podcast411/411_060325.mp3" length="11779397" type="audio/mpeg"/>
-    //</item>
 }

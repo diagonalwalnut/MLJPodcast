65fc6cad165f4b892186dae485ed53714fcddf3e
Thad Hoskins <thadhoskins@live.com>
2016-12-14 13:26:22 +0100

    RSS fix from https://devstuffs.wordpress.com/2014/02/07/first-line-empty-in-razor/

---
+++ mljPodcast/Models/RazorFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace MyWebsite.Helpers
+{
+    public class RazorXmlViewPage<T> : WebViewPage<T>
+    {
+        public override void ExecutePageHierarchy()
+        {
+            Response.ContentType = "text/xml";
+            Layout = null;
+            PushContext(new WebPageContext(), Response.Output); // push a new page context to the stack
+            base.ExecutePageHierarchy();
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+
+    public class RazorXmlViewPage : RazorXmlViewPage<object>
+    {
+
+    }
+}
